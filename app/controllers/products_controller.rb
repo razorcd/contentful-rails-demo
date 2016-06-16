8201dca7cb0c3258ad7cfbe0348e7819e9ccbdad
razor <razorcd@yahoo.com>
2016-06-17 01:55:26 +0200

    adds sorting to porducts index controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    render json: Product.all,
+    render json: Product.order(:created_at).all,
         only: [:id, :name, :remote_id, :created_at, :updated_at, :slug, :description,
            :size_type_color, :price, :quantity, :sku, :website],
         methods: [:tag_values]
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe ProductsController, type: :controller do
   context "#index" do
     it "should return a list of products" do
-      expect(Product).to receive(:all).and_return([{id: "123", name: "name123"}])
+      expect(Product).to receive_message_chain(:order, :all).and_return([{id: "123", name: "name123"}])
 
       get :index
       expect(response.body).to eq "[{\"id\":\"123\",\"name\":\"name123\"}]"
