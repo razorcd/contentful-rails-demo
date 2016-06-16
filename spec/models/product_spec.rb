f3fb22b0b4fa17f85284498187597757d0160f2f
razor <razorcd@yahoo.com>
2016-06-17 01:21:00 +0200

    adds Tag model specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe Product, type: :model do
     all_columns = Product.columns.map(&:name)
     expect(all_columns).to match(
         ["id", "name", "remote_id", "created_at", "updated_at", "slug", "description",
-         "size_type_color", "tags", "price", "quantity", "sku", "website"])
+         "size_type_color", "price", "quantity", "sku", "website"])
+  end
+
+  it "should have associations" do
+    associations = Product.reflect_on_all_associations
+    expect(associations.map(&:name)).to match [:tags]
+    expect(associations.map(&:class)).to eq [ActiveRecord::Reflection::HasAndBelongsToManyReflection]
   end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -2,7 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
   it "should have fields" do
-    all_columns = Product.columns.map(&:name)
+    all_columns = Tag.columns.map(&:name)
     expect(all_columns).to match(["id", "value", "created_at", "updated_at"])
+  end
+
+  it "should have associations" do
+    associations = Tag.reflect_on_all_associations
+    expect(associations.map(&:name)).to match [:product]
+    expect(associations.map(&:class)).to eq [ActiveRecord::Reflection::HasAndBelongsToManyReflection]
   end
 end
