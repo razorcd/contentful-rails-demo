321a0964fa3235b7bbfc32762c9191a7d1e39c91
razor <razorcd@yahoo.com>
2016-06-16 13:55:14 +0200

    adds dotenv

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'rest-client', '~> 1.8.0'
 
+gem 'dotenv-rails', '~> 2.1.1'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/services/contentful_sync_protocol.rb
+++ app/services/contentful_sync_protocol.rb
@@ -1,11 +1,12 @@
 require 'rest_client'
-ACCESS_TOKEN = ""
-SPACE = ""
+ACCESS_TOKEN = ENV["ACCESS_TOKEN"]
+SPACE = ENV["SPACE"]
+CONTENT_TYPE=ENV["CONTENT_TYPE"]
 
 class ContentfulSyncProtocol
   def sync_data
     initial="true"
     type="Deletion"
-    RestClient.get "https://cdn.contentful.com/spaces/#{SPACE}/sync?access_token=#{ACCESS_TOKEN}&initial=#{initial}&type=#{type}"
+    RestClient.get "https://cdn.contentful.com/spaces/#{SPACE}/sync?access_token=#{ACCESS_TOKEN}&initial=#{initial}&type=#{type}&content_type=#{CONTENT_TYPE}"
   end
 end
