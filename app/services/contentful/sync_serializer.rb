f3c9fd6f9f6cdb34cf4b186bee5fcfdcdd65c968
razor <razorcd@yahoo.com>
2016-06-17 22:57:13 +0200

    sync serializer digs trough hashes

--- app/services/contentful/sync_serializer.rb
+++ app/services/contentful/sync_serializer.rb
@@ -1,8 +1,10 @@
 module Contentful::SyncSerializer
   def self.item item
-    if item["sys"]["type"] == "Entry" && item["sys"]["contentType"]["sys"]["id"] == ENV["PRODUCT_CONTENT_TYPE"]
+    type = item.dig("sys", "type")
+    content_type_id = item.dig("sys", "contentType", "sys", "id")
+    if type == "Entry" && content_type_id == ENV["PRODUCT_CONTENT_TYPE"]
       return self.product_entry item
-    elsif item["sys"]["type"] == "DeletedEntry"
+    elsif type == "DeletedEntry"
       return self.deletion item
     else
       return nil
@@ -13,22 +15,22 @@ module Contentful::SyncSerializer
     {
       type: :entry,
       id: item["sys"]["id"],
-      name: item["fields"]["productName"]["en-US"],
-      slug: item["fields"]["slug"]["en-US"],
-      description: item["fields"]["productDescription"]["en-US"],
-      size_type_color: item["fields"]["sizetypecolor"]["en-US"],
-      tags: item["fields"]["tags"]["en-US"],
-      price: item["fields"]["price"]["en-US"],
-      quantity: item["fields"]["quantity"]["en-US"],
-      sku: item["fields"]["sku"]["en-US"],
-      website: item["fields"]["website"]["en-US"],
+      name: item.dig("fields", "productName", "en-US"),
+      slug: item.dig("fields", "slug", "en-US"),
+      description: item.dig("fields", "productDescription", "en-US") || "",
+      size_type_color: item.dig("fields", "sizetypecolor", "en-US") || "",
+      tags: item.dig("fields", "tags", "en-US") || [],
+      price: item.dig("fields", "price", "en-US"),
+      quantity: item.dig("fields", "quantity", "en-US") || 0,
+      sku: item.dig("fields", "sku", "en-US") || "",
+      website: item.dig("fields", "website", "en-US") || "",
     }
   end
 
   def self.deletion item
     {
-      # type: :deleted_entry,
-      # id: item["sys"]["id"],
+      type: :deleted_entry,
+      id: item["sys"]["id"],
     }
   end
 end
